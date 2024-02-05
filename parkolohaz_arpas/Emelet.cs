0b71981f92ef0bfcbbe249acd9cdc43dd2f0930a
Kevin <arpaskevin11@gmail.com>
2024-02-05 14:05:57 +0100

    Add project files.

---
+++ parkolohaz_arpas/Emelet.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace parkolohaz_arpas
+{
+    internal class Emelet
+    {
+        public int Szint { get; set; }
+        public string SzintNev { get; set; }
+        public List<int> Szektorok { get; set; }
+
+        public Emelet(string sor, int index)
+        {
+            var d = sor.Split("; ");
+            Szint = index+1;
+            SzintNev = d[0];
+            Szektorok = new List<int>();
+            for (int i = 1; i < d.Length; i++)
+            {
+                Szektorok.Add(int.Parse(d[i]));
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"{SzintNev,11} | {string.Join(", ", Szektorok)}";
+        }
+    }
+}
---
+++ parkolohaz_arpas/Program.cs
@@ -0,0 +1,57 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace parkolohaz_arpas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Emelet> emeletek = new();
+            StreamReader sr = new(@"..\..\..\src\parkolohaz.txt");
+
+            int index = 0;
+
+            while (!sr.EndOfStream)
+            {
+                emeletek.Add(new(sr.ReadLine(), index));
+                index++;
+            }
+
+            Console.WriteLine("7. feladat:\n");
+
+            Console.WriteLine("Szint neve: | 1. szektor | 2. szektor | 3. szektor | 4. szektor | 5. szektor | 6. szektor");
+            foreach (var item in emeletek)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine($"\n8. feladat: A(z) {emeletenkevesebbAuto(emeletek).Szint}. emeleten van a legkevesebb autó.");
+
+            foreach (var (a,b) in nincsAutoSzektorok(emeletek)) {  Console.WriteLine($"{a}. Emelet {b}. szektor"); }
+
+
+            Console.ReadKey();
+        }
+
+        static Emelet emeletenkevesebbAuto(List<Emelet> l) => l.MinBy(e => e.Szektorok.Sum());
+        static Dictionary<int,int> nincsAutoSzektorok(List<Emelet> l)
+        {
+            Dictionary<int, int> dict = new Dictionary<int, int>();
+            var emeletenNincsAuto = l.Where(e => e.Szektorok.Contains(0));
+
+            foreach (var e in emeletenNincsAuto)
+            {
+                for (int i = 0; i < e.Szektorok.Count; i++)
+                {
+                    if (e.Szektorok[i] == 0)
+                    {
+                        dict.Add(e.Szint, i);
+                    }
+                }
+            }
+
+            return dict;
+            
+        }
+    }
+}
